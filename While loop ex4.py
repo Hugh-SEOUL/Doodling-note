59b2d072113a351aededbe1806f5e694da1cae2e
Hugh-SEOUL <hughseoul@gmail.com>
2022-01-13 23:32:00 +0900

    Coding Trace

---
+++ While loop ex4.py
@@ -0,0 +1,52 @@
+# i = 0
+
+# while i < 10:
+#     print(i, end= " ")
+#     i += 1
+
+
+# i=1
+# num=0
+# while i <= 10:
+#     num += i
+#     i += 1
+# print("1~10까지의 누계합 : ", num )
+
+
+
+# i = 1
+# fa= 1
+
+# while i <= 5 :
+#     fa *= i
+#     i += 1
+# print("5! : ", fa)
+
+
+
+
+# i = 1
+# while i <= 9 :
+#     print("3*",i,"=",3*i)
+#     i += 1
+
+
+# i =1
+# hap = 0
+
+# while i <= 100 :
+#     if (i % 3) == 0:
+#         hap += i
+#     i += 1
+# print(hap)
+        
+
+# num = 1234
+# hap = 0
+
+# while  num > 0 :
+#     digit = num % 10
+#     hap += digit
+#     num = num // 10
+
+# print("1234 정수의 자리 수의 합은 %d 입니다." % hap)
