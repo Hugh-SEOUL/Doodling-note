dafaa1b84fd1f80b7083101e32e9d4b9ea6a0c13
[Hugh_SEOUL] <hughseoul@gmail.com>
2022-02-19 18:21:11 +0900

    gg

---
+++ 2022_0219_lotto.py
@@ -0,0 +1,15 @@
+
+
+
+from random import *
+
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+
+date_ = randint(4,28)
+
+print("오프라인 스터디 모임 날짜는 매월",date_,"일로 선정 되었습니다.")
