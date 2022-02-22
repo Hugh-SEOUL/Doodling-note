bdcee392f6bcba3181d2d4f181d6f32177ff16a4
[Hugh_SEOUL] <hughseoul@gmail.com>
2022-02-22 23:30:05 +0900

    g

---
+++ 220222_01.py
@@ -0,0 +1,6 @@
+
+gkrwja = int(input("학점을 입력하세요: "))
+vudwja = float(input("평점을 입력하세요 :"))
+
+if gkrwja > 140 and vudwja > 2.0:
+    print("해당 학생의 학점은 {0}이며 평점이 {1}이기에 졸업을 할 수 있습니다.".format(gkrwja, vudwja))
