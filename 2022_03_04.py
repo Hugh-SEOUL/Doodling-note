5e4c5dca5d4f452c7c48f76752dcdda481232972
[Hugh_SEOUL] <hughseoul@gmail.com>
2022-03-05 20:27:54 +0900

    gg

---
+++ 2022_03_04.py
@@ -0,0 +1,17 @@
+# for i in [0,1,2,3,4] :
+#     print("안녕하세요")
+    
+
+# for x in range(5):
+#     print("안녕하세요")
+    
+    
+# for x in [ 0,1,2,3,4]:
+#     print(x,end="\t")
+
+
+# sum = 0 
+# for x in range(10):
+#     sum += x
+# print("0에서 9까지의 누계합 : {0}".format(sum))
+
