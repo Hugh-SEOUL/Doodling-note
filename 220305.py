518ef46c8befc914ede4126b49381c638967f66d
[Hugh_SEOUL] <hughseoul@gmail.com>
2022-03-07 00:07:08 +0900

    gg

---
+++ 220305.py
@@ -0,0 +1,35 @@
+
+
+
+# # num = int(input("숫자를 입력하세요: "))
+# # sum=0
+
+# # for i in range(num):
+# #     sum += i
+# # print(sum)
+
+
+
+
+# sum=0
+
+
+# for i in range(101):
+#     if sum >= 2000:
+#         break
+#     sum += i
+# print(sum)
+
+
+# fact = 1.0
+# num = int(input("숫자를 입력하세요"))
+
+# for i in range(1,num+1):
+#     fact *= i
+# print(fact)
+
+num = int(input("출력하고 싶은 단"))
+
+for i in range(1,10):
+    print(num,"*",i,"=",(num*i))
+    
